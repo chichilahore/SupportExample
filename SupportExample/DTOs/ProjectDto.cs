12c53e8dbe4e87cef28fdc6f40365f1f432b3a01
Chichilahore <alexandrchichiloza@gmail.com>
2024-06-20 12:23:33 +0200

    create migrations and usersecrets

---
+++ SupportExample/DTOs/ProjectDto.cs
@@ -0,0 +1,12 @@
+namespace SupportExample.DTOs;
+
+public class ProjectDto
+{
+    public int Id { get; set; }
+    [Required, StringLength(50)]
+    public string Name { get; set; }
+    [Required, StringLength(400)]
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; } = null!;
+}
--- SupportExample/Program.cs
+++ SupportExample/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SupportExample.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+
+    var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
+    conStrBuilder.Password = builder.Configuration["DbPassword"];
+    var connection = conStrBuilder.ConnectionString;
+    options.UseSqlServer(connection);
+
+});
 
 var app = builder.Build();
 
@@ -12,6 +29,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    builder.Configuration.AddUserSecrets<Program>();
 }
 
 app.UseHttpsRedirection();
